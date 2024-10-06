0ccd4d5c2fd160b092e3d8eb69649d245d608cfc
yanzimsg <dxy231520@gmail.com>
2024-10-06 16:25:40 +0800

    Create jd_sc.js

---
+++ rewrite/jd_sc.js
@@ -0,0 +1,14 @@
+// Quantumult X Rewrite Script
+// [rewrite_local]
+// ^https:\/\/api\.m\.jd\.com\/client\.action\?functionId=batchReceiveXFCoupon url script-response-body jd_modify_response.js
+
+// [mitm]
+// hostname = api.m.jd.com
+
+// jd_modify_response.js
+let body = $response.body;
+
+// 将返回的 "status":"8" 修改为 "status":"0"
+body = body.replace(/"status":"8"/g, '"status":"0"');
+
+$done({ body });
