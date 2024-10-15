ee6a44b8a3aafe38168ff3741f8900001a9708e1
yanzimsg <dxy231520@gmail.com>
2024-10-15 12:41:25 +0800

    Update baishiCk.js

--- rewrite/baishiCk.js
+++ rewrite/baishiCk.js
@@ -1,15 +1,20 @@
 // [rewrite_local]
-// ^https:\/\/2024blueutc\.pepsi\.com\.cn\/person\/getPrizeList\.do\?type=GGL&pageNum=1 url script-response-body get_token.js
+// ^https:\/\/2024blueutc\.pepsi\.com\.cn\/person\/getPrizeList\.do\?type=GGL&pageNum=1 url script-request-header get_token.js
 
 // get_token.js
-
 let url = $request.url;
-$notify('Token 提取', 'url:', url);
+
 // 提取 token 参数
 let tokenMatch = url.match(/token=([^&]*)/);
-let token = tokenMatch ? decodeURIComponent(tokenMatch[1]) : '未找到 token';
+let token = tokenMatch ? tokenMatch[1] : '未找到 token';
+
+// URL 解码
+let decodedToken = decodeURIComponent(token);
+
+// 将解码后的 token 放入剪贴板
+$clipboard = decodedToken;
 
 // 发送通知
-$notify('Token 提取', '获取到的 token:', token);
+$notify('Token 提取', '获取到的 token:', decodedToken);
 
-$done({ body });
+$done({});
