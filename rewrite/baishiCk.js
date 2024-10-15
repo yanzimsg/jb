950b6f9d597c20b77d9899e74e322f90674d19d6
yanzimsg <dxy231520@gmail.com>
2024-10-15 12:32:17 +0800

    Update baishiCk.js

--- rewrite/baishiCk.js
+++ rewrite/baishiCk.js
@@ -2,9 +2,9 @@
 // ^https:\/\/2024blueutc\.pepsi\.com\.cn\/person\/getPrizeList\.do\?type=GGL&pageNum=1 url script-response-body get_token.js
 
 // get_token.js
-let body = $response.body;
-let url = $request.url;
 
+let url = $request.url;
+$notify('Token 提取', 'url:', url);
 // 提取 token 参数
 let tokenMatch = url.match(/token=([^&]*)/);
 let token = tokenMatch ? decodeURIComponent(tokenMatch[1]) : '未找到 token';
