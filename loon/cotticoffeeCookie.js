e987ec29c318d906b03e40a26a5eda51333e9e7a
yanzimsg <dxy231520@gmail.com>
2024-10-22 14:53:50 +0800

    Update cotticoffeeCookie.js

--- loon/cotticoffeeCookie.js
+++ loon/cotticoffeeCookie.js
@@ -2,7 +2,7 @@ const cookieName = '库迪咖啡'
 const token = $request.headers['token']
 const body = $request.body;
 let mobile = '未找到 mobile 参数';
-
+$notify('库迪Cookie 获取成功', 'token:', mobile + "----" + token);
 if (body) {
     // 解析 body 中的 mobile 参数
     const bodyObj = JSON.parse(body);  // 假设请求体是 JSON 格式，如果是其他格式请自行调整解析逻辑
