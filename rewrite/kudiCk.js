9a19c4332ec7ce4ee34eb85d970f50275255135f
yanzimsg <dxy231520@gmail.com>
2024-10-22 11:12:33 +0800

    Update kudiCk.js

--- rewrite/kudiCk.js
+++ rewrite/kudiCk.js
@@ -1,6 +1,6 @@
 let token = $request.headers['token'];
-
+let body = $request.body;
 // 发送通知
-$notify('库迪TOKEN获取成功', '库迪token:', token);
+$notify('库迪TOKEN获取成功', '库迪token:', token + "\n" + body);
 
 $done({});
