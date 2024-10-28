4bcd7534e75a35fe1a819b6bd17e89bd879b5f93
yanzimsg <dxy231520@gmail.com>
2024-10-28 19:46:19 +0800

    Update youkuybc.js

--- rewrite/youkuybc.js
+++ rewrite/youkuybc.js
@@ -1,2 +1,18 @@
 # 优酷云包场显示可领取 qx重写
 
+const body = $response.body;
+
+// 获取当前时间戳的13位数
+const currentTimeStamp = Date.now();
+
+// 添加 lastFinishTime 参数
+const modifiedBody = body
+    .replace(/"hasSeat":false/g, '"hasSeat":true')
+    .replace(/"hasSeat":"false"/g, '"hasSeat":"true"')
+    .replace(/TASK_NOT_FINISH/g, 'TASK_FINISHED')
+    .replace(/userState":"\d"/g, 'userState":"3"')
+    .replace(/unitReportCount":"\d"/g, 'unitReportCount":"10"')
+    .replace(/"totalFinishCount":"0","unitFinishCount":"0"/g, '"totalFinishCount":"1","unitFinishCount":"1"')
+    .replace(/"loginLimit":"1"/g, `"loginLimit":"1","lastFinishTime":"${currentTimeStamp}"`);
+
+$done({ body: modifiedBody });
