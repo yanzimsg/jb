4071d38bdc0d68cd93d925161c3e91ab4fdbff86
yanzimsg <dxy231520@gmail.com>
2024-11-04 16:30:30 +0800

    Update chagee_cookie.js

--- loon/chagee_cookie.js
+++ loon/chagee_cookie.js
@@ -1,24 +1,20 @@
-
-let body = $request.body;
-
+const cookieName = '霸王茶姬'
+const body = $request.body;
 
 if (body) {
     // 解析 body 
-    let bodyObj = JSON.parse(body);  // 假设请求体是 JSON 格式，如果是其他格式请自行调整解析逻辑
-    let userId = bodyObj.userInfo.id;
-    let userMobile = bodyObj.userInfo.mobile;
+    const bodyObj = JSON.parse(body);  // 假设请求体是 JSON 格式，如果是其他格式请自行调整解析逻辑
+    const userId = bodyObj.userInfo.id;
+    const userMobile = bodyObj.userInfo.mobile;
     // 获取 userToken
-    let userToken = bodyObj.userToken;
-}
-
-// 如果获取到了 Cookie
-if (userId && userMobile && userToken) {
-
-    $notification.post('茶姬Cookie', '获取成功', '')
-    console.log("茶姬Cookie完整CK：" + "\n\n" + userMobile + "----" + userId + "----"+ userToken)
-    
-    // 保存Cookie到本地存储（可选）
-    // $persistentStore.write(cookie, cookieName);
+    const userToken = bodyObj.userToken;
+	if (userToken && userId && userMobile) {
+        $notification.post(cookieName, '获取Cookie成功', '')
+        console.log(cookieName +"获取到完整CK：" + "\n\n\n\n" + userMobile + "----" + userId + "----"+ userToken + "----电脑抓key和iv \n")
+      
+        // 保存Cookie到本地存储（可选）
+        // $persistentStore.write(cookie, cookieName);
+    }
 } 
 
 $done({});
