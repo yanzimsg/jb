5270ad08eb92957bf54f7433ba127727efb20fd5
yanzimsg <dxy231520@gmail.com>
2024-11-08 19:47:11 +0800

    Update youkuybc.js

--- loon/youkuybc.js
+++ loon/youkuybc.js
@@ -1,17 +1,17 @@
 $notification.post('优酷云包场', '修改返回', '')
 
-let currentTimeStamp = Date.now();
+//let currentTimeStamp = Date.now();
 
 let body = $response.body;
 // 添加 lastFinishTime 参数
 let modifiedBody = body
     .replace(/"hasSeat":false/g, '"hasSeat":true')
-    .replace(/"hasSeat":"false"/g, '"hasSeat":"true"')
-    .replace(/TASK_NOT_FINISH/g, 'TASK_FINISHED')
-    .replace(/userState":"\d"/g, 'userState":"3"')
-    .replace(/unitReportCount":"\d"/g, 'unitReportCount":"10"')
-    .replace(/"totalFinishCount":"0","unitFinishCount":"0"/g, '"totalFinishCount":"1","unitFinishCount":"1"')
-    .replace(/"loginLimit":"1"/g, `"loginLimit":"1","lastFinishTime":"${currentTimeStamp}"`);
+    .replace(/"hasSeat":"false"/g, '"hasSeat":"true"');
+    //.replace(/TASK_NOT_FINISH/g, 'TASK_FINISHED')
+    //.replace(/userState":"\d"/g, 'userState":"3"')
+    //.replace(/unitReportCount":"\d"/g, 'unitReportCount":"10"')
+    //.replace(/"totalFinishCount":"0","unitFinishCount":"0"/g, '"totalFinishCount":"1","unitFinishCount":"1"')
+    //.replace(/"loginLimit":"1"/g, `"loginLimit":"1","lastFinishTime":"${currentTimeStamp}"`);
 // $notify(modifiedBody);
 
 console.log(modifiedBody)
